098772aa3192f462e04fdf46bb5a8ad91fd0a3c6
zahra <zahratee@gmail.com>
2016-02-20 01:00:35 +0000

    Cleaned up the JS files and added comments.

--- popup.js
+++ popup.js
@@ -3,8 +3,10 @@ function submitKeys() {
 	var c_id = document.getElementById('client_id').value;
 	var c_secret = document.getElementById('client_secret').value;
 
+
+	//First script executed defines keys for use in content.js
   	chrome.tabs.executeScript({
-	    code: "var client_details = ['" + c_id + "', '" + c_secret + "'];"
+	    code: "var keys = ['" + c_id + "', '" + c_secret + "'];"
 	}, function() {
 	    chrome.tabs.executeScript({file: 'content.js'});
 	});
